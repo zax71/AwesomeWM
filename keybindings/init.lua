96d9ca2d92f174fb868be93b588a27a76bfc99cb
Zax71 <67716263+zax71@users.noreply.github.com>
2025-01-10 18:52:16 +0000

    We have volume control again

--- keybindings/init.lua
+++ keybindings/init.lua
@@ -17,6 +17,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
+local volume_widget = require("awesome-wm-widgets.volume-widget.volume")
 -- {{{ Key bindings
 
 local modkey = "Mod4"
